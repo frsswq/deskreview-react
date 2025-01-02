aac1ad960242219d4021e81f27fc1a470889bbbf
frsswq <farissaifuddin.work@gmail.com>
2025-01-02 19:21:37 +0700

    beautify manifesto text

--- src/components/common/styles/GlobalStyles.tsx
+++ src/components/common/styles/GlobalStyles.tsx
@@ -4,6 +4,9 @@ const GlobalStyles = createGlobalStyle`
     /* import fonts */
     @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
 
     /* global styles */
     * {
--- src/components/home/styles/AboutStyled.tsx
+++ src/components/home/styles/AboutStyled.tsx
@@ -7,23 +7,22 @@ export const AboutManifesto = styled.div`
   margin: 0 auto;
 
   h1 {
-    font-family: "Fraunces", "sans-serif";
-    font-weight: 300;
+    font-family: "EB Garamond", "sans-serif";
+    font-weight: 400;
     font-style: italic;
-    font-size: 50px;
-    letter-spacing: -0.05em;
-    margin-bottom: 10px;
+    font-size: 80px;
+    letter-spacing: -0.075em;
   }
 
   p {
     font-size: 20px;
     font-weight: 300;
-    letter-spacing: 0em;
-    line-height: 1.2;
+    letter-spacing: -0.025em;
+    line-height: 1.6;
     word-wrap: break-word;
     overflow-wrap: break-word;
     max-width: 60ch;
-    margin-bottom: 10px;
+    margin-bottom: 15px;
   }
 
   blockquote {
