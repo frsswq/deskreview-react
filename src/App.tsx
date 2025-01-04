874c2c2f3e82c55b4dbfe84e125eedbaa9471969
frsswq <farissaifuddin.work@gmail.com>
2025-01-04 23:06:02 +0700

    make github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import GlobalStyles from "./components/common/styles/GlobalStyles.tsx";
 import Home from "./pages/Home.tsx";
 import Blog from "./pages/Blog.tsx";
@@ -8,11 +8,15 @@ export default function App() {
   return (
     <>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/study" element={<Blog />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <BrowserRouter
+        basename={import.meta.env.DEV ? "/" : "/deskreview-react/"}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/study" element={<Blog />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [
+      react({
+        babel: {
+          plugins: ["babel-plugin-styled-components"],
+        },
+      }),
+    ],
+    base: "/",
+  };
 
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ["babel-plugin-styled-components"],
-      },
-    }),
-  ],
+  if (command !== "serve") {
+    config.base = "/deskreview-react/";
+  }
+
+  return config;
 });
