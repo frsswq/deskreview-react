f54bd3859462deb0ad3e48f15be4f9e95b7e4105
frsswq <farissaifuddin.work@gmail.com>
2025-01-06 23:59:53 +0700

    optimize css font import and delete unused images

--- src/components/common/styles/GlobalStyles.tsx
+++ src/components/common/styles/GlobalStyles.tsx
@@ -1,85 +1,85 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyles = createGlobalStyle`  
-    /* colours */
-    :root {
-        --desk-gray-300: 234, 236, 236;
-        --desk-gray-500: 187, 189, 191;
-        --desk-gray-700: 52, 53, 53;
-        --desk-blue: 46, 89, 169;
-        --desk-brown: 87, 74, 67;
-        --desk-orange: 227, 108, 42;
-        --desk-white: 255, 255, 255;
-        --desk-black: 5, 5, 5;
-    }
-    
-    /* font sizes */
-    :root {
-        --text-xs: 0.75rem;
-        --text-sm: 0.875rem;
-        --text-base: 1rem;
-        --text-lg: 1.125rem;
-        --text-xl: 1.25rem;
-        --text-2xl: 1.375rem;
-        --text-3xl: 1.5rem;
-    }
-    
-    /* others */
-    :root {
-        --max-w: 600px;
-    }
+const GlobalStyles = createGlobalStyle`
+  /* colours */
+  :root {
+    --desk-gray-300: 234, 236, 236;
+    --desk-gray-500: 187, 189, 191;
+    --desk-gray-700: 52, 53, 53;
+    --desk-blue: 46, 89, 169;
+    --desk-brown: 87, 74, 67;
+    --desk-orange: 227, 108, 42;
+    --desk-white: 255, 255, 255;
+    --desk-black: 5, 5, 5;
+  }
 
-    /* global styles */
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
+  /* font sizes */
+  :root {
+    --text-xs: 0.75rem;
+    --text-sm: 0.875rem;
+    --text-base: 1rem;
+    --text-lg: 1.125rem;
+    --text-xl: 1.25rem;
+    --text-2xl: 1.375rem;
+    --text-3xl: 1.5rem;
+  }
 
-    body {
-        background-color: #ffffff;
-        overflow-y: scroll;
-        scrollbar-gutter: stable;
-    }
+  /* others */
+  :root {
+    --max-w: 600px;
+  }
 
-    p, li {
-        font-family: 'DM Sans', sans-serif;
-        font-weight: 300;
-        font-size: var(--text-xl);
-        line-height: 1.333;
-        letter-spacing: -0.025em;
-        
-        @media (width <= 768px) {
-            font-size: var(--text-base);
-        }      
-    }
+  /* global styles */
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
-    li {
-        list-style-type: none;
-    }
+  body {
+    background-color: #ffffff;
+    overflow-y: scroll;
+    scrollbar-gutter: stable;
+  }
 
-    main {
-        padding: 0 16px;
-    }
+  p,
+  li {
+    font-family: "DM Sans", sans-serif;
+    font-weight: 300;
+    font-size: var(--text-xl);
+    line-height: 1.333;
+    letter-spacing: -0.025em;
 
-    /* scrollbar */
-    body::-webkit-scrollbar {
-        width: 0.8vw;
+    @media (width <= 768px) {
+      font-size: var(--text-base);
     }
- 
-    body::-webkit-scrollbar-track {
-        background-color: rgb(var(--desk-gray-300));
-    }
-    
-    body::-webkit-scrollbar-thumb {
-        background-color: rgba(var(--desk-gray-500), 0.8);
-        border-radius: 1em;
-    
+  }
 
-        &:hover {
-            background-color: rgba(var(--desk-gray-500), 1);
-        }
+  li {
+    list-style-type: none;
+  }
+
+  main {
+    padding: 0 16px;
+  }
+
+  /* scrollbar */
+  body::-webkit-scrollbar {
+    width: 0.8vw;
+  }
+
+  body::-webkit-scrollbar-track {
+    background-color: rgb(var(--desk-gray-300));
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: rgba(var(--desk-gray-500), 0.8);
+    border-radius: 1em;
+
+    &:hover {
+      background-color: rgba(var(--desk-gray-500), 1);
     }
+  } 
 `;
 
 export default GlobalStyles;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
-import "@fontsource/dm-sans/300.css";
-import "@fontsource/dm-sans/400.css";
-import "@fontsource/dm-sans/500.css";
-import "@fontsource/eb-garamond/400.css";
-import "@fontsource/eb-garamond/400-italic.css";
-import "@fontsource/eb-garamond/500-italic.css";
+import "@fontsource/dm-sans/latin-300.css";
+import "@fontsource/dm-sans/latin-400.css";
+import "@fontsource/dm-sans/latin-500.css";
+import "@fontsource/eb-garamond/latin-400.css";
+import "@fontsource/eb-garamond/latin-400-italic.css";
+import "@fontsource/eb-garamond/latin-500-italic.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
