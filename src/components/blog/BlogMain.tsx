712020494d2ea37da0055d7014a7ba40c25cd50d
frsswq <farissaifuddin.work@gmail.com>
2025-01-09 01:18:44 +0700

    try use md instead of mdx

--- src/components/blog/BlogMain.tsx
+++ src/components/blog/BlogMain.tsx
@@ -9,7 +9,25 @@ import { blogPageData } from "../../data/blog/blogPage";
 import { blogPageDataTypes } from "../../types/homeTypes";
 import BlogButton from "./BlogButton";
 
+// interface MDXModule {
+//   default: React.ComponentType;
+// }
+
 export default function BlogMain() {
+  // const mdxFiles = import.meta.glob<MDXModule>("../contents/*.mdx", {
+  //   eager: true,
+  // });
+
+  // const blogLinks = Object.entries(mdxFiles).map(([path, module]) => {
+  //   const fileName = path.match(/\/([^/]+)\.mdx$/)?.[1] || "";
+  //   const title = module.title || fileName;
+
+  //   return {
+  //     path: fileName,
+  //     title: title,
+  //   };
+  // });
+
   return (
     <MainStyled>
       <SectionContainer>
